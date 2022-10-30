28239888373299ddffcca26cf2f385f4301dd422
Mike Stok <(none)>
2022-10-30 10:03:40 -0400

    Add my fish config file
    Initial chezmoi experiment.

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,20 @@
+if status is-interactive
+		set -g fish_autosuggestion_enabled 0
+    set fish_greeting '' # turn off the greeting
+end
+
+if test -d /usr/local/sbin
+    fish_add_path /usr/local/sbin
+end
+
+if test -d $HOME/local/bin
+    fish_add_path $HOME/local/bin
+end
+
+set -x EDITOR vim
+
+set -x ERL_AFLAGS "-kernel shell_history enabled"
+set -x KERL_BUILD_DOCS true
+set -x KERL_CONFIGURE_OPTIONS --without-javac
+
+source (brew --prefix asdf)/libexec/asdf.fish
